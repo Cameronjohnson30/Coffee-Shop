98a1069f6df615769e29e54147cdf8534c1f7d37
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-08-14 00:01:41 -0700

    add function inventory details and proptypes

---
+++ src/components/InventoryDetails.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from 'prop-types'
+
+function InventoryDetails(props){
+  const { product, onClickingDelete } = props;
+  return (
+    <React.Fragment>
+      <h1>Iventory Details</h1>
+      <h2> {product.name}</h2>
+      <h2> {product.origin}</h2>
+      <h3> {product.price}</h3>
+      <h3> {product.roast}</h3>
+      <h3> {product.qauntity}</h3>
+      <button onClick={() => props.onClickingRestock(product.id) }>Restock</button>
+      <button onClick={ props.onClickingEdit }>Update Product</button>
+      <button onClick={() => onClickingDelete(product.id)}> Delete Product</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+InventoryDetails.propTypes = {
+  product: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func,
+  onClickingRestock: PropTypes.func,
+}
+export default InventoryDetails;
