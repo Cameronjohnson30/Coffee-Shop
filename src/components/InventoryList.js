ed4e9ced9fabc9cd71feaa769e008c6be4184f0f
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-08-14 00:00:21 -0700

    add function iventory list and inventorylist proptypes

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -1,5 +1,32 @@
 import React from "react";
+import Product from "./Product";
 import PropTypes from "prop-types";
 
+
+
+function InventoryList(props){
+
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.productList.map((product) =>
+        <Product
+          whenProductClicked = { props.onProductSelection }
+          name={product.name}
+          origin={product.origin}
+          price={product.price}
+          roast= {product.roast}
+          quantity= {product.quantity}
+          id={product.id}
+          key={product.id}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+InventoryList.propTypes = {
+  productList: PropTypes.array,
+  onProductSelection: PropTypes.func
+};
 
 export default InventoryList;
